674ad4a403e6405e0a1fc571f4f5aaae276bcfa9
Anastasiia Sanina <saninaanstasia97@gmail.com>
2022-06-27 22:15:53 +0200

    Fix parse

--- engine/parser.go
+++ engine/parser.go
@@ -15,16 +15,18 @@ func Parse(commandLine string) Command {
     if len(parts) == 2 {
       return &toPrint{arg: parts[1]}
     } else {
-      return &toPrint{arg: "Error"}
+      return &toPrint{arg: "Error. Check arguments"}
     }
 	case "split":
 		if len(parts) > 3 {
-      return &toPrint{arg: "Error"}
+      return &toPrint{arg: "Error. Too many arguments"}
+    } else if len(parts) < 2 {
+      return &toPrint{arg: "Error. Too less arguments"}
     }
 		arg1 := parts[1]
 		arg2 := parts[2]
 		return splitText(arg1, arg2)
 	default:
-		return &toPrint{arg: "Error"}
+		return &toPrint{arg: "Error occured"}
 	}
 }
