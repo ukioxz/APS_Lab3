f355dc2347c89b5e8bcc0b15c01140583a8d904b
Anastasiia Sanina <saninaanstasia97@gmail.com>
2022-06-27 16:21:44 +0200

    Add errors to parser

--- engine/parser.go
+++ engine/parser.go
@@ -6,14 +6,21 @@ import (
 
 func Parse(commandLine string) Command {
 	parts := strings.Fields(commandLine)
+	if(len(parts) == 0) {
+		return &toPrint{arg: "Error. Too less arguments."}
+	}
+
 	switch parts[0] {
 	case "print":
-    if len(parts) > 2 {
-      return &toPrint{arg: "Error"}
-    } else {
+    if len(parts) == 2 {
       return &toPrint{arg: parts[1]}
+    } else {
+      return &toPrint{arg: "Error"}
     }
 	case "split":
+		if len(parts) > 3 {
+      return &toPrint{arg: "Error"}
+    }
 		arg1 := parts[1]
 		arg2 := parts[2]
 		return splitText(arg1, arg2)
