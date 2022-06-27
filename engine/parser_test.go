1b108ef18efa37c7fd9614e4d23ffc7680537656
ukioxz <ikatember@gmail.com>
2022-06-27 17:35:05 +0300

    Add tests for split

--- engine/parser_test.go
+++ engine/parser_test.go
@@ -22,3 +22,35 @@ func TestParsePrint3(s *testing.T) {
 	res:= Parse(commandTxt)
 	assert.Equal(s, &toPrint{arg: "Error"}, res)
 }
+
+func TestParseSplit1(s *testing.T) {
+  commandTxt := "split go,the,best ,"
+	res:= Parse(commandTxt)
+  res2:= splitText("go,the,best", ",")
+	assert.Equal(s, res2, res)
+}
+
+func TestParseSplit2(s *testing.T) {
+  commandTxt := "splitttt go,the,best ,"
+	res:= Parse(commandTxt)
+	assert.Equal(s, &toPrint{arg: "Error"}, res)
+}
+
+func TestParseSplit3(s *testing.T) {
+  commandTxt := "split go,the,best . , 5"
+	res:= Parse(commandTxt)
+	assert.Equal(s, &toPrint{arg: "Error"}, res)
+}
+
+func TestParseSplit4(s *testing.T) {
+  commandTxt := "split '3''goda''do''zavoda' ''"
+	res:= Parse(commandTxt)
+  res2:= splitText("'3''goda''do''zavoda'", "''")
+	assert.Equal(s, res2, res)
+}
+
+func TestParseSplit5(s *testing.T) {
+  commandTxt := ""
+	res:= Parse(commandTxt)
+	assert.Equal(s, &toPrint{arg: "Error. Too less arguments."}, res)
+}
